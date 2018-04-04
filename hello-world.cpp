54e4c104710ef318bde38500608be5d78a98792e
Nguyen Khang <nguyenkhacnguyenkhang@gmail.com>
2018-04-04 23:46:05 +0700

    new update!

--- hello-world.cpp
+++ hello-world.cpp
@@ -1,11 +1,15 @@
 #include<iostream>
+#include<cstdint>
+#include<limits.h>
+#include<cstring>
+#include<cmath>
 //#include "hello-world.h"
 using namespace std;
 
 class QInt
 {
 private:
-  long int Bits[2];
+  int64_t Bits[2];
 public:
   QInt();
   ~QInt();
@@ -13,6 +17,13 @@ public:
 
 int main()
 {
-  cout<<"Hello world!\n";
+  int64_t value = 0;
+  string a="123456";
+  int length= a.length();
+  for(int i = 0; i < length; i++)
+  {
+    value = value + (a[length-i-1]-'0')*(int)pow(10,i);
+  }
+  cout<<value <<endl;
   return 0;
 }
---
+++ hello-world.h
@@ -0,0 +1,6 @@
+#pragma #ifndef _HELLO_WORLD
+#define _HELLO_WORLD
+
+
+
+#endif /* end of include guard: _HELLO-WORLD */
