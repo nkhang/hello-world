7ba8a7131bbcc22215124c76682355392534d63d
Khang. Nguyen Khac Nguyen <nguyenkhacnguyenkhang@gmail.com>
2020-09-28 21:57:13 +0700

    add (CI): setting up

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/nkhang/pprstats
+
+go 1.15
+
+require github.com/gin-gonic/gin v1.6.3
---
+++ main.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+	"log"
+
+	"github.com/gin-gonic/gin"
+)
+
+func main() {
+	r := gin.Default()
+	r.GET("/", func(c *gin.Context) {
+		c.String(200, "Hello World")
+	})
+	r.Run()
+	log.Println("Hello World")
+}
