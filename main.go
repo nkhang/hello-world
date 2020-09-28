19c10a2a010f254093ad32a718c4e854d9e1aada
Khang. Nguyen Khac Nguyen <nguyenkhacnguyenkhang@gmail.com>
2020-09-28 23:17:36 +0700

    add (view): add view models

--- main.go
+++ main.go
@@ -8,8 +8,13 @@ import (
 
 func main() {
 	r := gin.Default()
+	r.LoadHTMLGlob("views/*")
 	r.GET("/", func(c *gin.Context) {
-		c.String(200, "Hello World")
+		// c.String(200, "Hello World")
+		name := c.Query("name")
+		c.HTML(200, "index.tmpl", map[string]interface{}{
+			"name": name,
+		})
 	})
 	r.Run()
 	log.Println("Hello World")
